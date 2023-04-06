490199c3af07390243ab5a62bbe225b36e5b3c57
Arvind Battula <arvind.battula@nblenergy.com>
2023-04-06 15:12:41 -0500

    auto generated code for integer and float ops

---
+++ Basics/Hindi/13_testing.py
@@ -0,0 +1,37 @@
+# Example for float and integer operations
+# Addition
+a = 5
+b = 2.5
+c = a + b  # float + integer = float
+d = b + a  # integer + float = float
+print(c)  # Output: 7.5
+print(d)  # Output: 7.5
+
+# Subtraction
+e = 7.5
+f = 3
+g = e - f  # float - integer = float
+h = f - e  # integer - float = float
+print(g)  # Output: 4.5
+print(h)  # Output: -4.5
+
+# Multiplication
+i = 2
+j = 3.5
+k = i * j  # integer * float = float
+l = j * i  # float * integer = float
+print(k)  # Output: 7.0
+print(l)  # Output: 7.0
+
+# Division
+m = 10
+n = 3
+o = m / n  # integer / integer = float
+p = n / m  # integer / integer = float
+q = m / j  # integer / float = float
+r = j / m  # float / integer = float
+print(o)  # Output: 3.3333333333333335
+print(p)  # Output: 0.3
+print(q)  # Output: 2.857142857142857
+print(r)  # Output: 0.35
+
